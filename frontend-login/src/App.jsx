1fbee23ba3c78755799015f7ec19a2a35f9b6f4d
dhvani1403 <dhvanishah140304@gmail.com>
2025-02-22 19:31:46 +0530

    App.jsx updated

--- frontend-login/src/App.jsx
+++ frontend-login/src/App.jsx
@@ -4,23 +4,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./Login.jsx";
 import AdminDashboard from "./components/AdminDashboard.jsx";
-import UserDashboard from "./components/UserDashboard.jsx";
+import UserPage from "./components/UserPage.jsx";
+import AddExpense from "./components/AddExpense";
+import ViewHistory from "./components/ViewHistory.jsx";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-       <Routes>
-        <Route path="/" element={<Register />} /> {/* Default route */}
+      <Routes>
+        <Route path="/" element={<Register />} /> 
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/user" element={<UserDashboard />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} /> {/* Catch-all route */}
+
+        {/* User Page with Nested Routes */}
+        <Route path="/user" element={<UserPage />}>
+          <Route path="add-expense" element={<AddExpense />} />
+          <Route path="view-history" element={<ViewHistory />} />
+          <Route index element={<AddExpense />} /> {/* Default to Add Expense */}
+        </Route>
+
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
